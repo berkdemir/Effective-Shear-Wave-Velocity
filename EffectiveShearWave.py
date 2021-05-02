b9ca67d336c4be46f63257eb1d93835ebe40f53e
Berk Demir <40423004+berkdemir@users.noreply.github.com>
2021-05-02 15:43:13 +0300

    Update EffectiveShearWave.py

--- EffectiveShearWave.py
+++ EffectiveShearWave.py
@@ -1,17 +1,27 @@
-#  Effective Shear Wave Velocity Using Darendeli (2001) and Schnabel (1973) by Berk Demir / https://github.com/berkdemir
-
-# INPUTS
-Ground_Type = "Soil"  # Either "Soil" or "Rock"
-PGV = 80  # cm/sec
-PGV_Red = 0.8  # PGV reduction ratio with depth
-VS = 300  # Maximum shear wave velocity, m/s
-PI = 20  # Plasticity index in percent. (For Soil only.)
-OCR = 1  # Overconsolidation Ratio. (For Soil only.)
-Eff_Pressure = 200  # Effective pressure in kPa. (For Soil only)
-
-
-def EFS(Ground_Type, PGV, PGV_Red, VS, PI, OCR, Eff_Pressure):
-    # CALCULATIONS
+def EFS(Ground_Type, PGV, PGV_Red, VS, PI=20, OCR=1, Eff_Pressure=300):
+    """
+    Effective Shear Wave Velocity Using Darendeli (2001) and Schnabel (1973) by Berk Demir / https://github.com/berkdemir
+    
+    Inputs:
+        Ground_Type: Either "Soil" or "Rock"
+        PGV: Peak Ground Velocity (cm/sec) 
+        PGV_Red: PGV reduction ratio with depth
+        VS: Maximum shear wave velocity (m/s)
+        PI: Plasticity index in percent. (For Soil only.)
+        OCR: Overconsolidation Ratio. (For Soil only.)
+        Eff_Pressure: Effective pressure (kPa) (For Soil only)
+        
+    Returns:
+        VS_Red: Reduction ratio for maximum shear wave velocity.
+    
+    Notes:
+        Also prints a sentence with reduction ratio and effective shear wave velocity.
+        
+    Example:
+        For Soils: Vs_Red = EFS(Ground_Type = "Soil", PGV = 70, PGV_Red = 0.8, VS = 300, PI=20, OCR=1, Eff_Pressure=300)
+        For Rocks: Vs_Red = EFS(Ground_Type = "Rock", PGV = 70, PGV_Red = 0.8, VS = 800)
+    """
+    
     PGV_eff = PGV * PGV_Red * 0.01  # m/s
     Shear_Modulus_Reaction = 0.7  # initial value
     VS_Red = pow(Shear_Modulus_Reaction, 0.5)
